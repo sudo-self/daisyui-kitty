fb9d15db3d30ee729e9563dc0ad3fa291c7b0c35
sudo-self <sudo-self@proton.me>
2024-01-04 06:51:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-sveltekit-65aeg5
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sudo-self/daisyui-sveltekit-65aeg5)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+
+<script>
+    let hereKitty = true;
+
+    const handleMouseenter = () => (hereKitty = true);
+    const handleMouseleave = () => (hereKitty = false);
+</script>
+
+<svelte:body on:mouseenter={handleMouseenter} on:mouseleave={handleMouseleave} />
+
+<img
+    class:curious={hereKitty}
+    alt="Kitten wants to know what's going on?"
+    src="https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/cat.png"
+/>
+
+<style>
+    img {
+        position: absolute;
+        left: 0;
+        bottom: -60px;
+        transform: translate(-80%, 0) rotate(-30deg);
+        transform-origin: 100% 100%;
+        transition: transform 0.4s;
+    }
+
+    .curious {
+        transform: translate(-15%, 0) rotate(0deg);
+    }
+
+    :global(body) {
+        overflow: hidden;
+    }
+</style>
